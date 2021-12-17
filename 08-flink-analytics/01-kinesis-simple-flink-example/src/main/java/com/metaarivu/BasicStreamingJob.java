1357fa7b526a15910e625b8347b44f34c4ff1b1f
ketangote <ketan.gote@metamagic.in>
2021-12-17 12:33:58 +0530

    Changes: Stream changes..

---
+++ 08-flink-analytics/01-kinesis-simple-flink-example/src/main/java/com/metaarivu/BasicStreamingJob.java
@@ -0,0 +1,56 @@
+package com.metaarivu;
+
+import java.util.Properties;
+
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kinesis.FlinkKinesisConsumer;
+import org.apache.flink.streaming.connectors.kinesis.FlinkKinesisProducer;
+import org.apache.flink.streaming.connectors.kinesis.config.ConsumerConfigConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A basic Kinesis Data Analytics for Java application with Kinesis data streams
+ * as source and sink.
+ */
+public class BasicStreamingJob {
+	private static final String region = "ap-south-1";
+	private static final String inputStreamName = "stock-input-stream";
+	private static final String outputStreamName = "stock-output-stream";
+
+	private static final Logger log = LoggerFactory.getLogger(BasicStreamingJob.class);
+
+	private static DataStream<String> createSourceFromStaticConfig(StreamExecutionEnvironment env) {
+		Properties inputProperties = new Properties();
+		inputProperties.setProperty(ConsumerConfigConstants.AWS_REGION, region);
+		inputProperties.setProperty(ConsumerConfigConstants.STREAM_INITIAL_POSITION, "LATEST");
+
+		return env.addSource(new FlinkKinesisConsumer<>(inputStreamName, new SimpleStringSchema(), inputProperties));
+	}
+
+	private static FlinkKinesisProducer<String> createSinkFromStaticConfig() {
+		Properties outputProperties = new Properties();
+		outputProperties.setProperty(ConsumerConfigConstants.AWS_REGION, region);
+		outputProperties.setProperty("AggregationEnabled", "false");
+
+		FlinkKinesisProducer<String> sink = new FlinkKinesisProducer<>(new SimpleStringSchema(), outputProperties);
+		sink.setDefaultStream(outputStreamName);
+		sink.setDefaultPartition("0");
+		return sink;
+	}
+
+	public static void main(String[] args) throws Exception {
+		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+		DataStream<String> input = createSourceFromStaticConfig(env);
+
+		input.map(value -> {
+			log.info("DATACHANGED=>{}", value);
+			return value;
+		}).addSink(createSinkFromStaticConfig());
+
+		env.execute("Flink Stock Streaming Java API Skeleton");
+	}
+}
---
+++ 08-flink-analytics/02-flink-installation/README.md
@@ -0,0 +1,10 @@
+# Installing Flink Dashboard 
+
+- Download and extract TAR from  [flink site](https://flink.apache.org/downloads.html)
+- Start Flink
+    - ./bin/start-cluster.sh
+    - <img width="1672" alt="Screen Shot 2021-12-16 at 8 30 40 PM" src="https://user-images.githubusercontent.com/23295769/146395659-e947d54c-8478-4a63-9f3f-73acb020d390.png">
+
+
+- Stop Flink
+    - ./bin/stop-cluster.sh
