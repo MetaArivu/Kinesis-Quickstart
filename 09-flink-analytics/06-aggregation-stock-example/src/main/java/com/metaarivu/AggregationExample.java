8d76574f15adf094a36603bbd4a70157b6c0ef3f
ketangote <ketan.gote@metamagic.in>
2021-12-20 14:23:18 +0530

    Logic Updated..

--- 09-flink-analytics/06-aggregation-stock-example/src/main/java/com/metaarivu/AggregationExample.java
+++ 09-flink-analytics/06-aggregation-stock-example/src/main/java/com/metaarivu/AggregationExample.java
@@ -12,45 +12,36 @@ public class AggregationExample {
 		System.out.println("Starting Stream Stock Trend Example ==>");
 		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-		DataStream<Tuple5<String, String, String, Double, String>> stocks = env.socketTextStream("localhost", 9991)
+		DataStream<Tuple5<String, String, String, Double, Double>> stocks = env.socketTextStream("localhost", 9991)
 				.map(new StockSplitter());
 
-		SingleOutputStreamOperator<Tuple5<String, String, String, Double, String>> stockTrend = 
-				 stocks.keyBy(v -> v.f2)
-				.reduce(new StockReducer());
+		SingleOutputStreamOperator<Tuple5<String, String, String, Double, Double>> stockReduced = stocks
+				.keyBy(v -> v.f1).reduce(new StockReducer());
 
-		stockTrend.print();
+		stockReduced.print();
 
 		env.execute("Stream Stock Trend Example");
 
 	}
 
-	public static class StockSplitter implements MapFunction<String, Tuple5<String, String, String, Double, String>> {
-		public Tuple5<String, String, String, Double, String> map(String value) // 1,HDFC,BANKING,1450
+	public static class StockSplitter implements MapFunction<String, Tuple5<String, String, String, Double, Double>> {
+		public Tuple5<String, String, String, Double, Double> map(String value) // 1,HDFC,BANKING,1450
 		{
 			String[] stock = value.split(","); // words = [{1},{HDFC},{BANKING},{1450}
-			return new Tuple5<String, String, String, Double, String>(stock[0], stock[1], stock[2],
-					Double.valueOf(stock[3]), "NA");
+			return new Tuple5<String, String, String, Double, Double>(stock[0], stock[1], stock[2],
+					Double.valueOf(stock[3]), 0d);
 		}
 	}
 
-	public static class StockReducer implements ReduceFunction<Tuple5<String, String, String, Double, String>> {
+	public static class StockReducer implements ReduceFunction<Tuple5<String, String, String, Double, Double>> {
 
 		@Override
-		public Tuple5<String, String, String, Double, String> reduce(
-				Tuple5<String, String, String, Double, String> current,
-				Tuple5<String, String, String, Double, String> prev) throws Exception {
+		public Tuple5<String, String, String, Double, Double> reduce(
+				Tuple5<String, String, String, Double, Double> current,
+				Tuple5<String, String, String, Double, Double> prev) throws Exception {
 
-			String trend = "NA";
-			if (current.f3 != null && prev.f3 != null) {
-				if (current.f3.doubleValue() == prev.f3.doubleValue()) {
-					trend = "Same";
-				} else
-					trend = current.f3.doubleValue() > prev.f3.doubleValue() ? "Increased" : "Decreased";
-			}
-
-			return new Tuple5<String, String, String, Double, String>(current.f0, current.f1, current.f2, current.f3,
-					trend);
+			return new Tuple5<String, String, String, Double, Double>(current.f0, current.f1, current.f2, current.f3,
+					prev.f3);
 		}
 
 	}
